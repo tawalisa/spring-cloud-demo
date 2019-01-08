4f59a80527e84521dd5953ed1a9c9a00d7786c69
lij <tawalisa@163.com>
2019-01-08 15:02:26 +0800

    add properties

--- BuildDocker.sh
+++ BuildDocker.sh
@@ -1,4 +1,5 @@
-cd demo-config && docker build -t tawalisa/config .
+cd demo-config/config && git init && git add . && git -c user.name='tawalisa' -c user.email='tawalisa@163.com' commit -m "initial"
+cd ../../demo-config && docker build -t tawalisa/config .
 cd ../demo-eureka && docker build -t tawalisa/eureka .
 cd ../demo-feign && docker build -t tawalisa/feign .
 cd ../demo-service && docker build -t tawalisa/service .
